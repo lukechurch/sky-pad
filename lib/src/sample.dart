168f34026cfdd33671815f4b416a24ff80a63e79
Luke Church <lukechurch@google.com>
2015-05-12 14:00:39 +0100

    Add piping and infrastructure

--- lib/src/sample.dart
+++ lib/src/sample.dart
@@ -5,11 +5,14 @@
 library dart_pad.sample;
 
 final String dartCode = r'''
+#!mojo mojo:sky_viewer
+<script>
 void main() {
-  for (int i = 0; i < 5; i++) {
-    print('hello ${i + 1}');
+  for (int i = 0; i < 4; i++) {
+    print('hello ${i}');
   }
 }
+</script>
 ''';
 
 final String htmlCode = '\n';
